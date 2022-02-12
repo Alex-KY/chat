6619ac1fb5f9da4934cb819e4aac880f5809c070
Alexey Kravchenko <a.kravchenko@rt-solar.ru>
2022-02-12 21:25:32 +0300

    Добавлен сервер nodeJS

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Это файл readme.md
+<h1 align="center">ChatUp</h1>
+<p align="center"><img src="https://img.shields.io/badge/nodeJS->=v12.22.10-success.svg" /></p>
+
+## Описание
+Это учебный проект Яндекс.Практикума первого модуля курса Мидл фронтенд-разработчик.
+
+## Демо
+<h2 align="center"><a  href="https://yandex-praktikum-middle-dev-chat.netlify.app">Netlify</a></h2>
+
+## Установка
+- npm i
+- npm run serve
--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@ const PORT = 3000;
 
 const app = express();
 
-app.use('/', express.static(__dirname));
+app.use(express.static('dist'));
 
-app.use('/404', express.static(`${__dirname}/views/404/404.html`));
-
-app.get(/\/*/, express.static(`${__dirname}/views/404/404.html`));
+app.use('/login', express.static('dist'));
+app.use('/signup', express.static('dist'));
+app.use('/chat', express.static('dist'));
+app.use('/profile', express.static('dist'));
+app.use('/404', express.static('dist'));
+app.use('/500', express.static('dist'));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
